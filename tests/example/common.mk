96899530849f1cad030c5b873878af1fe0ed9ad7
Sichun Qin <sichunqin@hotmail.com>
2022-12-26 18:48:57 +0800

    modify make file to work on linux enviroment

--- tests/example/common.mk
+++ tests/example/common.mk
@@ -1,14 +1,14 @@
 
 RISCV_PATH := $(TOOLCHAIN_DIR)/tools/gnu-mcu-eclipse-riscv-none-gcc-8.2.0-2.2-20190521-0004-win64/
 
-RISCV_GCC     := $(abspath $(RISCV_PATH)/bin/riscv-none-embed-gcc)
-RISCV_AS      := $(abspath $(RISCV_PATH)/bin/riscv-none-embed-as)
-RISCV_GXX     := $(abspath $(RISCV_PATH)/bin/riscv-none-embed-g++)
-RISCV_OBJDUMP := $(abspath $(RISCV_PATH)/bin/riscv-none-embed-objdump)
-RISCV_GDB     := $(abspath $(RISCV_PATH)/bin/riscv-none-embed-gdb)
-RISCV_AR      := $(abspath $(RISCV_PATH)/bin/riscv-none-embed-ar)
-RISCV_OBJCOPY := $(abspath $(RISCV_PATH)/bin/riscv-none-embed-objcopy)
-RISCV_READELF := $(abspath $(RISCV_PATH)/bin/riscv-none-embed-readelf)
+RISCV_GCC     := riscv64-unknown-elf-gcc
+RISCV_AS      := riscv64-unknown-elf-as
+RISCV_GXX     := riscv64-unknown-elf-g++
+RISCV_OBJDUMP := riscv64-unknown-elf-objdump
+RISCV_GDB     := riscv64-unknown-elf-gdb
+RISCV_AR      := riscv64-unknown-elf-ar
+RISCV_OBJCOPY := riscv64-unknown-elf-objcopy
+RISCV_READELF := riscv64-unknown-elf-readelf
 
 .PHONY: all
 all: $(TARGET)
